47cb6992a7413d947c91bba6a8f1c5192376198c
Pragna Matta <147132957+pragna262@users.noreply.github.com>
2024-12-08 12:04:49 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,8 @@
 public class Sample{
   public static void main(String[] args){
-    System.out.println("Helooo");
+    for(int i=0;i<3;i++){
+      System.out.print(i+1 + " ");
+    }
+    System.out.println("Goo");
   }
 }
